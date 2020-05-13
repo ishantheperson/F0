48bbe3dba392015d22b084ea6420817a55c695ca
Ishan Bhargava <ishantheperson@users.noreply.github.com>
2020-05-12 21:48:52 -0700

    added -s flag and delete C1 file on success

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,7 @@ module Main where
 import Control.Monad 
 
 import System.Environment
+import System.Directory
 import System.FilePath.Posix
 import System.Process 
 
@@ -28,6 +29,7 @@ data CompilerOptions = CompilerOptions
     printTypes :: Bool,
     printTransformed :: Bool,
     executeProgram :: Bool,
+    keepC1 :: Bool,
     file :: FilePath
   }
 
@@ -35,7 +37,8 @@ compilerOptions = CompilerOptions
   <$> Opts.switch (Opts.long "print-ast" <> Opts.help "print out the AST after parsing")
   <*> Opts.switch (Opts.long "print-types" <> Opts.help "print out the types of the top level decls")
   <*> Opts.switch (Opts.long "print-transformed" <> Opts.help "print out the transformed program")
-  <*> Opts.switch (Opts.long "execute" <> Opts.short 'x' <> Opts.help "execute the program if it compiels")
+  <*> Opts.switch (Opts.long "execute" <> Opts.short 'x' <> Opts.help "execute the program if it compiles")
+  <*> Opts.switch (Opts.long "save-files" <> Opts.short 's' <> Opts.help "save the generated C1 code")
   <*> (Opts.argument Opts.str (Opts.metavar "<input file>"))
 
 options = Opts.info (compilerOptions Opts.<**> Opts.helper) Opts.fullDesc
@@ -83,11 +86,16 @@ main = do
 
   when (printTransformed options) $ pPrint c0Program
 
+  -- Write output file 
   let basename = dropExtension inputFile
       outputFileName = basename ++ ".c1"
   writeFile outputFileName (outputProgram c0Program)
 
   -- Compile program using CC0 
   callProcess "cc0" ["-o", basename, outputFileName] 
-  when (executeProgram options) $ 
-    callProcess ("./" ++ basename) []
+
+  -- Avoid cluttering the directory by removing the generated code
+  unless (keepC1 options) $ removeFile outputFileName
+
+  -- If requested, execute the program
+  when (executeProgram options) $ callProcess ("./" ++ basename) []
